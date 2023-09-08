d40f111e77a53b7b740e115ecbc55b3d837a00d3
57397 <57397@etu.he2b.be>
2023-09-08 02:17:51 +0200

    refactor code + code comment

--- models/table.py
+++ models/table.py
@@ -1,23 +1,14 @@
 import flet as ft
-import os
 
-__author__ = "Fatih AKMAN"
-__status__ = "TEST"
 
-"""
-Script GUI de CARIBOU avec flet
-Usage: uitest.py
-"""
-
-def main(page: ft.Page):
-    # INITALISATION DES TABLEAUX
-    def tableInit():
+class TableControl(ft.UserControl):
+    def build(self):
         lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
 
         lv.controls.append(
             ft.ResponsiveRow(
                 [
-                    ft.Column(col={"sm": 6}, controls=[ft.Container(
+                        ft.Column(col={"sm": 6}, controls=[ft.Container(
                         content=ft.DataTable(
                             columns=[
                                 ft.DataColumn(ft.Text("First name")),
@@ -93,7 +84,7 @@ def main(page: ft.Page):
                         bgcolor=ft.colors.AMBER,
 
                         border_radius=10,
-                    ),]), ft.Column(col={"sm": 6}, controls=[ft.Container(
+                    ),]),ft.Column(col={"sm": 6}, controls=[ft.Container(
                         content=ft.DataTable(
                             columns=[
                                 ft.DataColumn(ft.Text("First name")),
@@ -130,7 +121,7 @@ def main(page: ft.Page):
                         bgcolor=ft.colors.GREEN,
 
                         border_radius=10,
-                    ),]), ft.Column(col={"sm": 6}, controls=[ft.Container(
+                    ),]),ft.Column(col={"sm": 6}, controls=[ft.Container(
                         content=ft.DataTable(
                             columns=[
                                 ft.DataColumn(ft.Text("First name")),
@@ -206,79 +197,4 @@ def main(page: ft.Page):
                         border_radius=10,
                     ),]),
                 ]))
-        page.add(lv)
-        page.update()
-
-    # CHANGE LA VALEUR DU CONTROLLEUR QUI AFFICHE LE FICHIER SELECTIONNE
-    def dropdown_changed(e):
-        selectedFiletoString.value = f"Vous avez sélectionné le fichier : {dd.value}"
-        page.update()
-    # RETOURNE UNE LISTE DES FICHIERS PRESENT DANS '../files/'
-
-    def getFiles():
-        fileSet = set()
-        for root, dirs, files in os.walk('files'):
-            for fileName in files:
-                fileSet.add(os.path.join(root[len('files'):], fileName))
-
-        print(fileSet)
-        fileOption = []
-        for file in files:
-            fileOption.append(ft.PopupMenuItem(text=file))
-        fileOption.append(ft.PopupMenuItem())
-        return fileOption
-
-    # INITALISATION APPBAR
-    def appBarInit():
-        page.appbar = ft.AppBar(
-            leading=ft.Icon(ft.icons.DONUT_SMALL_OUTLINED),
-            leading_width=40,
-            title=ft.Text("CARIBOU V2"),
-            center_title=False,
-            bgcolor=ft.colors.SURFACE_VARIANT,
-        )
-
-    # INITALISATION BODY
-    def bodyInit():
-        page.add(ft.Container(
-            content=ft.Text("Veuillez sélectionner un fichier"),
-            padding=ft.Padding(6, 6, 0, 0),))
-
-        selectedFileContainer = ft.Container(
-            content=ft.Column([selectedFiletoString]),
-            padding=ft.Padding(6, 0, 0, 0),
-        )
-
-        configContainer = ft.Container(
-            content=ft.Column([ft.ResponsiveRow([
-                ft.Column(col={"sm": 6}, controls=[dd]),
-                ft.Column(col={"sm": 2}, controls=[ft.ElevatedButton(text="Export", color=ft.colors.BLACK),
-                                                   ]),
-                ft.Column(col={"sm": 2}, controls=[ft.ElevatedButton(text="Submit", color=ft.colors.BLACK)])])]),
-            padding=6,
-        )
-
-        searchContainer = ft.Container(
-            content=ft.TextField(label="Faire une recherche"),
-            padding=6,
-        )
-        page.add(configContainer, selectedFileContainer, searchContainer)
-        page.update()
-        
-    selectedFiletoString = ft.Text()
-    fileOption = getFiles()
-
-    dd = ft.Dropdown(
-        on_change=dropdown_changed,
-        options=fileOption,
-        width=200,
-    )
-
-    appBarInit()
-    bodyInit()
-    tableInit()
-
-
-# ENTRY POINT
-if __name__ == "__main__":
-    ft.app(target=main)
+        return lv
